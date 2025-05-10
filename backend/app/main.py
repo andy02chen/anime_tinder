11f214e31b80aa4c64fb37a914097ef2a4b7cfce
Andy Chen <andy02chen@gmail.com>
2025-05-10 11:44:02 +1000

    Store oauth stuff in DB

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 import models, schemas
-from database import engine
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 from dotenv import load_dotenv
 import os
+import base64
+import hashlib
+import secrets
+from models import OAuthSession, Users
 
 app = FastAPI()
 
@@ -16,20 +21,65 @@ app.add_middleware(
 
 load_dotenv()
 
+API_URL = os.getenv("OAUTH_REDIRECT_URI")
+CLIENT_ID = os.getenv("OAUTH_CLIENT_ID")
+CLIENT_SECRET = os.getenv("OAUTH_CLIENT_SECRET")
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
 # Create table on start up
 @app.on_event("startup")
 def startup():
     models.Base.metadata.create_all(bind=engine)
 
+
 # Auth
 @app.get("/oauth")
-def oauth_login():
+async def oauth_login(db: Session = Depends(get_db)):
 
+    # Generate token for oauth
+    user_session = generate_session_token()
+    oauth_state = generate_state() # TODO store in frontend
+    code_verifier = generate_code_verifier()
+    code_challenge = generate_code_challenge(code_verifier)
+
+    # Store values
+    store_oauth_session(db, user_session, oauth_state, code_verifier)
 
     return {"message": "Start OAuth"}
 
+# Store the Auth session
+def store_oauth_session(db: Session, session_id: str, state: str, code_verifier: str):
+    oauth = OAuthSession(
+        session_id=session_id,
+        oauth_state=state,
+        code_verifier=code_verifier
+    )
+    db.add(oauth)
+    db.commit()
+
+# Generate random state
+def generate_state(length: int = 32) -> str:
+    import secrets
+    return secrets.token_urlsafe(length)[:length]
+
+# Generate a session token
+def generate_session_token(length: int = 64) -> str:
+    token = secrets.token_urlsafe(length)
+    return token[:length]
 
 # Generate Code Verifier
-
+def generate_code_verifier(length: int = 128) -> str:
+    verifier = secrets.token_urlsafe(length)
+    return verifier[:128]
 
 # Generate Code Challenge
+def generate_code_challenge(code_verifier: str) -> str:
+    sha256 = hashlib.sha256(code_verifier.encode("utf-8")).digest()
+    challenge = base64.urlsafe_b64encode(sha256).decode("utf-8").rstrip("=")
+    return challenge
--- backend/app/models.py
+++ backend/app/models.py
@@ -10,3 +10,10 @@ class Users(Base):
     refresh_token = Column(String)
     expires_in = Column(Integer)
     session_id = Column(String)
+
+class OAuthSession(Base):
+  __tablename__ = "oauth"
+
+  session_id = Column(String, unique=True)
+  oauth_state = Column(String, nullable=False, unique=True, primary_key=True)
+  code_verifier = Column(String, nullable=False)
